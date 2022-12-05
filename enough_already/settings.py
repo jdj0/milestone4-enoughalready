8fd1cdf858e97e7a7be318c7557668284306ccc7
jdj0 <joshua.davidjones92@gmail.com>
2022-12-05 15:36:06 +0000

    Add media file

--- enough_already/settings.py
+++ enough_already/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 from decouple import config
 
@@ -56,7 +58,9 @@ ROOT_URLCONF = 'enough_already.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
