35eda17f127e5905359fe2fd37bf5d613b2f29e1
jdj0 <joshua.davidjones92@gmail.com>
2023-01-22 15:20:49 +0000

    Add checkout app

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Item
 
@@ -14,13 +14,13 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ A view to add the item to the shopping bag """
 
-    item = Item.objects.get(pk=item_id)
+    item = get_object_or_404(Item, pk=item_id)
     item_quantity = item.quantity
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     bag[item_id] = item_quantity
-    messages.success(request, f'Added {item.title} to your bag')
+    messages.success(request, f'Added "{item.title}" to your bag')
 
     request.session['bag'] = bag
     print(request.session['bag'])
@@ -29,12 +29,16 @@ def add_to_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
+
     try:
+        item = get_object_or_404(Item, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Removed "{item.title}" from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
