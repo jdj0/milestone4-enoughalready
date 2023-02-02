9b1a19bd512daa898de926f49bf1b939cfcd6133
jdj0 <joshua.davidjones92@gmail.com>
2023-02-02 21:28:08 +0000

    Add delivery info form to account.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserAccount
+from .forms import UserAccountForm
 
 
 def account(request):
     """ Display user account template """
-
     account = get_object_or_404(UserAccount, user=request.user)
+
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST, instance=account)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your Account has been updated')
+
+    orders = account.orders.all()
+    form = UserAccountForm(instance=account)
 
     template = 'accounts/account.html'
     context = {
-        'account': account,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
