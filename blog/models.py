329bfebfe652c8c8b4e2ec54596982ce15defc87
jdj0 <joshua.davidjones92@gmail.com>
2023-03-12 14:45:00 +0000

    Add blog model in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from accounts.models import UserAccount
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=99)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+    published = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
