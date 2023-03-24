35dd5f6ba4fd72c20e8925c1a8efc7fe94ea6a09
jdj0 <joshua.davidjones92@gmail.com>
2023-03-24 20:09:13 +0000

    Change webhook handler in order to  debug error upon purchase

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -48,6 +48,9 @@ class Stripe_WH_Handler:
         """
         Handle payment_intent.succeeded
         """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
         intent = event.data.object
         pid = intent.id
         bag = intent.metadata.bag
